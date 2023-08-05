2249877b486b9038447aba1ab303c9fcfb334286
Arseny Karkotsky <karkohash@mail.ru>
2023-08-05 12:34:19 +0300

    feat: add animation burger and his functional

---
+++ library/js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("burger").addEventListener("click", function(){
+        document.querySelector(".header").classList.toggle("open")
+    })
+});
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("burg_butt").addEventListener("click", function(){
+        document.querySelector(".header").classList.toggle("open")
+    })
+});
+
+document.getElementById("menu").addEventListener('click', event => {
+    event._isClickWithUnMenu = true;
+});
+document.getElementById("burger").addEventListener('click', event => {
+    event._isClickWithUnMenu = true;
+});
+document.body.addEventListener('click', event => {
+    if (event._isClickWithUnMenu) return;
+    document.querySelector(".header").classList.remove("open")
+});
